9f16d4c0db83fc3721a624b1b3d562a0901d6732
Kensuke Fukutomi <145524808+kfukutom@users.noreply.github.com>
2025-02-07 14:34:11 -0500

    Initialize Sessions.tsx

---
+++ backend/api/Sessions.tsx
@@ -0,0 +1,22 @@
+import { promises } from "dns";
+import { getDatabase, ref, set } from "firebase/database";
+import { metadata } from "motion/react-client";
+
+// Session ID :
+function generateID(): string {
+    return Date.now().toString(36) + Math.random().toString(36).substring(2);
+}
+
+async function createSession(username: string): promies<void> {
+    const db = getDatabase();
+    const sessionID = generateID();
+
+    await set (ref(db, `sessions/${sessionID}`), {
+        sessionID: sessionID,
+        timestamp: new Date().toISOString(),
+    });
+    sessionStorage.setItem('sessionID', sessionID);
+    //return sessionID;
+}
+
+// Finish the Rest by Around End of Feb.
