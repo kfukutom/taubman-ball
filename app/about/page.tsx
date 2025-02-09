21baed419ad2a8395ece4a6ff0a0023fdb643ae0
Kensuke Fukutomi <145524808+kfukutom@users.noreply.github.com>
2025-02-08 20:35:05 -0500

    More Code Modularity

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -10,6 +10,7 @@ import event from "@/assets/events.jpg";
 import students from "@/assets/events.jpeg";
 import community from "@/assets/community.jpg";
 import { AnimatedTestimonials } from "@/components/ui/animated-testimonial";
+import Footer from "@/components/ui/Footer";
 
 export default function About() {
   const [currentTime, setCurrentTime] = useState("");
@@ -71,52 +72,7 @@ export default function About() {
         />
         </div>
       </main>
-      <footer className="bg-black w-full text-center py-8">
-        <p className="text-sm sm:text-md text-gray-300 max-w-lg mx-auto mb-4">
-          Join us to celebrate architecture, design, and creativity!
-        </p>
-        <p className="text-xs sm:text-sm text-gray-400">
-          <a
-            href="https://taubmancollege.umich.edu/"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="underline"
-          >
-            © 2025 Taubman College of Architecture and Urban Planning
-          </a>
-        </p>
-        <p className="text-xs sm:text-sm text-gray-400 mt-4">
-          <a
-            href="https://www.michigandaily.com/news/campus-life/inaugural-taubman-architecture-ball-celebrates-creativity-outside-the-classroom/"
-            target="_blank"
-            className="underline"
-          >
-            Past Event
-          </a>{" "}
-          | Currently, it's{" "}
-          <span className="text-blue-500 font-semibold">{currentTime}</span> |{" "}
-          <a href="mailto:kfukutom@umich.edu" className="underline">
-            Contact Us
-          </a>
-        </p>
-        <div className="flex justify-center items-center gap-6 mt-8">
-          <img
-            src={taubmanlogo.src}
-            alt="Taubman College of Architecture and Urban Planning"
-            className="w-20 cursor-pointer h-auto hover:scale-110 transform transition duration-300 ease-in-out"
-          />
-          <img
-            src={tab.src}
-            onClick = {
-                ()=>{
-                    router.push("/");
-                }
-            }
-            alt="TAB Logo"
-            className="w-20 cursor-pointer h-auto hover:scale-110 transform transition duration-300 ease-in-out"
-          />
-        </div>
-      </footer>
+      <Footer />
     </div>
   );
 }
