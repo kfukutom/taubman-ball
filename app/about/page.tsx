493743004898091710d651cec9f698712ffbb0eb
Kensuke Fukutomi <145524808+kfukutom@users.noreply.github.com>
2025-02-09 16:24:56 -0500

    Changes from Sunday, Styling

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -12,6 +12,15 @@ import community from "@/assets/community.jpg";
 import { AnimatedTestimonials } from "@/components/ui/animated-testimonial";
 import Footer from "@/components/ui/Footer";
 
+const emailID = "taubmanarchball@umich.edu";
+let descriptionEvent = (`TAB (Taubman Architecture Ball) is a celebration of architecture, design, and creativity, 
+hosted by the students of the Taubman College of Architecture and Urban Planning. 
+Join us for a night of inspiration, collaboration, and bonding.`);
+
+// Background components:
+import { StarsBackground } from "@/components/ui/stars-background";
+import { ShootingStars } from "@/components/ui/shooting-stars";
+
 export default function About() {
   const [currentTime, setCurrentTime] = useState("");
   const router = useRouter();
@@ -30,49 +39,86 @@ export default function About() {
   }, []);
 
   return (
-    <div className="min-h-screen bg-black text-white font-[family-name:var(--font-geist-sans)] flex flex-col">
-      <main className="flex-grow flex flex-col items-center justify-center p-10">
+    <div className="min-h-screen w-screen relative bg-black text-white overflow-hidden">
+      <StarsBackground 
+        className="absolute inset-0 z-0" 
+        starDensity={0.0054} 
+        allStarsTwinkle={false} 
+        twinkleProbability={1} 
+        minTwinkleSpeed={0.5} 
+        maxTwinkleSpeed={0.8} 
+      />
+      <ShootingStars 
+        className="absolute inset-0 z-0" 
+        minSpeed={5} 
+        maxSpeed={30} 
+        minDelay={3000} 
+        maxDelay={6000} 
+        starColor="#FFFFFF" 
+        trailColor="#9E00FF" 
+        starWidth={30} 
+        starHeight={2.25} 
+      />
+
+      {/* Main content goeshere */}
+      <div className="relative z-10 flex flex-col items-center justify-center p-10">
         <div className="max-w-3xl text-center">
-            <h1 className="text-4xl sm:text-5xl font-bold mb-6">
-                About <span className="text-blue-300">TAB</span>
-            </h1>
-            <p className="text-sm sm:text-xs text-gray-400 leading-relaxed pb-3">2000 Boinsteel Blvd, 700pm - 1000pm</p>
-                <p className="text-lg sm:text-xl text-gray-300 leading-relaxed mb-10">
-                    "TAB (Taubman Architecture Ball) is a celebration of architecture,
-                    design, and creativity, hosted by the students of the Taubman
-                    College of Architecture and Urban Planning. Join us for a night of
-                    inspiration, collaboration, and bonding."
-                </p>
-                
-            <p className="text-lg sm:text-xl text-gray-300 leading-relaxed mb-10">- sincerely, the <span className="font-mono">e</span>board :)</p>
-        <hr className="border-gray-600 my-10" />
-        {/* Animated Testimonal should go below */}
-        <AnimatedTestimonials
-          autoplay = {false}
-          testimonials={[
-            {
-              quote: "As a student-organized initiative, many plans executed on the night of the event were created by our wonderful students. We are proud to have such a talented and creative community, organizing this event once more.",
-              name: "",
-              designation: "A Student-Led Initiative",
-              src: students.src,
-            },
-            {
-              quote: "The Taubman Architecture Ball invites students, faculty, and staff to come together and celebrate the creativity and innovation that is at the heart of our community. We are beyond excited to have you here today with us, and there will be many exciting events coming up!",
-              name: "",
-              designation: "United as a Community",
-              src: community.src,
-            },
-            {
-              quote: "The event was previously a great success! We plan on continuing the tradition as the years go on! For now, join us for the night and let's celebrate together. And of course, there will be complimentary drinks as well as free food ;)",
-              name: "",
-              designation: "Past Event and Future Plans",
-              src: event.src,
-            }
-          ]}
-        />
+          <h1 className="text-4xl sm:text-5xl font-bold mb-6">
+            {/* This should be like a dim light */}
+            About <span className="text-white drop-shadow-[0_0_12px_rgba(255,255,255,0.8)]">TAB </span>
+          </h1>
+          <p className="text-sm sm:text-xs text-gray-400 leading-relaxed pb-3">
+            2000 Boinsteel Blvd, 700pm - 1000pm
+          </p>
+          <p className="text-lg sm:text-xl text-gray-300 leading-relaxed mb-5">
+            {descriptionEvent}
+          </p>
+          <p className="text-lg sm:text-xl text-gray-300 leading-relaxed mb-10">
+            - sincerely, the <span className="font-mono">e</span>board :)
+          </p>
+          <hr className="border-gray-600 my-10" />
+          {/* Animated testimonials: should go below */}
+          <h2 className="text-3xl sm:text-4xl pt-3 font-bold mb-4 
+          text-white drop-shadow-[0_0_12px_rgba(255,255,255,0.8)]">
+            Get Involved in Future Events
+          </h2>
+          <AnimatedTestimonials
+            autoplay={false}
+            testimonials={[
+              {
+                quote: "As a student-organized initiative, many plans executed on the night of the event were created by our wonderful students. We are proud to have such a talented and creative community, organizing this event once more.",
+                name: "",
+                designation: "A Student-Led Initiative",
+                src: students.src,
+              },
+              {
+                quote: "The Taubman Architecture Ball invites students, faculty, and staff to come together and celebrate the creativity and innovation that is at the heart of our community. We are beyond excited to have you here today with us, and there will be many exciting events coming up!",
+                name: "",
+                designation: "United as a Community",
+                src: community.src,
+              },
+              {
+                quote: "The event was previously a great success! We plan on continuing the tradition as the years go on! For now, join us for the night and let's celebrate together. And of course, there will be complimentary drinks as well as free food ;)",
+                name: "",
+                designation: "Past Event and Future Plans",
+                src: event.src,
+              },
+            ]}
+          />
+          <p className="text-md sm:text-md text-gray-300 leading-relaxed my-3">
+            We're always looking for new installation projects and would love new ideas to come on board with us!&nbsp;
+          </p>
+          <span className="text-gray-400"> If you're interested in joining our team, please reach out to us at </span>
+          <a 
+              href="mailto:taubmanarchball@umich.edu" 
+              className="text-align-center text-blue-400 hover:text-blue-300 underline transition duration-200"
+            >
+              {emailID}
+          </a>
+          <hr className="border-gray-600 my-10" />
         </div>
-      </main>
-      <Footer />
+      </div>
+      <Footer/>
     </div>
   );
 }
--- components/ui/input-output/Button-demo.tsx
+++ components/ui/input-output/Button-demo.tsx
@@ -10,6 +10,7 @@ interface ButtonProps {
 // directly reference to ./audio/button-default.mp3 here insteadd of using the audio tag
 const Button: React.FC<ButtonProps> = ({ label }) => {
   const audioRef = useRef<HTMLAudioElement | null>(null);
+  const [shake, setShake] = useState(false);
 
   useEffect(() => {
     audioRef.current = new Audio("/button-default.mp3");
@@ -26,10 +27,10 @@ const Button: React.FC<ButtonProps> = ({ label }) => {
   return (
     <button
       onClick={handleClick}
-      className="px-4 py-2 bg-gray-500 text-white rounded-lg 
-      hover:bg-gray-600 transition px-6"
+      className={`px-6 py-2 bg-gray-500 text-white rounded-lg 
+      hover:bg-gray-600 transition duration-200 ease-in-out 
+      ${shake ? "animate-shake" : ""}`}
     >
-      <audio ref={audioRef} src="/button-default.mp3" preload="auto"/>
       {label}
     </button>
   );
