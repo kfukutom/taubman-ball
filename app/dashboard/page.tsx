12a62bb89f91ed3aaf2dc95036985d31c12a0a72
Kensuke Fukutomi <145524808+kfukutom@users.noreply.github.com>
2025-02-13 13:19:37 -0500

    Fix SVG Padding

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -113,20 +113,21 @@ export default function Dashboard() {
             placeholder="Search responses..."
             value={searchQuery}
             onChange={(e) => setSearchQuery(e.target.value)}
-            className="w-full pl-12 pr-4 py-3 rounded-full text-gray-500 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-black-500 transition duration-300 shadow-md"
+            className="w-full pl-12 pr-4 py-3 rounded-full text-gray-500 placeholder-gray-500 focus:outline-none 
+            focus:ring-2 focus:ring-black-500 transition duration-300 shadow-md"
           />
           <svg
-            className="w-6 h-6 absolute top-1/2 left-8 sm:left-4 transform -translate-y-1/2 text-gray-500"
+            className="w-6 h-5 absolute top-10 left-8 sm:left-4 transform -translate-y-1/2 text-gray-500"
             fill="none"
             stroke="currentColor"
             viewBox="0 0 24 24"
             xmlns="http://www.w3.org/2000/svg"
           >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M21 21l-4.35-4.35M17 10a7 7 0 11-14 0 7 7 0 0114 0z"
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M21 21l-4.35-4.35M17 10a7 7 0 11-14 0 7 7 0 0114 0z"
             />
           </svg>
         </div>
